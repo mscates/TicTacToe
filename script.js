439df90fbfbcd2d0ab5b8bab9974580651da829c
Mark Cates <cates_mark@hotmail.com>
2019-03-09 13:46:52 -0600

    refactored checkWin and added functions for row column diagonal wins

--- script.js
+++ script.js
@@ -16,14 +16,13 @@ Tictactoe.prototype.addEventListeners = function () {
 Tictactoe.prototype.startGame = function (e) {
 
   if (e.target.innerHTML === 'X' || e.target.innerHTML === 'O') {
-    console.log('You can\'t do that');
   } else {
     e.target.innerHTML = this.turn;
     dataBox = e.target.getAttribute('id');
     this.letters.splice(dataBox, 1, this.turn);
     this.moves++;
     if (this.moves >= 5 && this.moves <= 9) {
-      this.checkWin(this.letters);
+      this.checkWin();
     }
     this.turn === 'X' ? this.turn = 'O' : this.turn = 'X';
     this.showTurn();
@@ -66,29 +65,38 @@ Tictactoe.prototype.showTurn = function () {
   whosTurn.innerHTML = 'Who\'s turn is it: ' + this.turn;
 }
 
-Tictactoe.prototype.checkWin = function (arr) {
-  let checkArray = [[[0, 1, 2], arr[0], arr[1], arr[2]],
-                    [[3,4,5], arr[3], arr[4], arr[5]],
-                    [[6,7,8], arr[6], arr[7], arr[8]],
-                    [[0,3,6], arr[0], arr[3], arr[6]],
-                    [[1,4,7], arr[1], arr[4], arr[7]],
-                    [[2,5,8], arr[2], arr[5], arr[8]],
-                    [[0,4,8], arr[0], arr[4], arr[8]],
-                    [[2,4,6], arr[2], arr[4], arr[6]]
-                    ];
-  for (let item of checkArray) {
-    let arrPossibles = item.slice(1);
-    answer = arrPossibles.reduce((a, b) => a === b ? a : false);
-    if (answer) {
-      console.log(`${answer} wins!`);
-      this.changeColor(item[0], 'red');
-      this.boxClicked.classList.add("container-no-click");
-      return;
-    }
-    else if (!answer && this.moves === 9) {
-      console.log('It is a tie!');
-      return;
-    }
+Tictactoe.prototype.rowWin = function (arr) {
+  return (arr[0] === arr[1] && arr[0] === arr[2] && arr[0] !== null) ||
+      (arr[3] === arr[4] && arr[3] === arr[5] && arr[3] !== null) ||
+      (arr[6] === arr[7] && arr[6] === arr[8] && arr[6] !== null)
+}
+
+Tictactoe.prototype.columnWin = function (arr) {
+  return (arr[0] === arr[3] && arr[0] === arr[6] && arr[0] !== null) ||
+      (arr[1] === arr[4] && arr[1] === arr[7] && arr[1] !== null) ||
+      (arr[2] === arr[5] && arr[2] === arr[8] && arr[2] !== null)
+}
+
+Tictactoe.prototype.diagonalWin = function (arr) {
+  return (arr[0] === arr[4] && arr[0] === arr[8] && arr[0] !== null) ||
+      (arr[2] === arr[4] && arr[2] === arr[6] && arr[2] !== null)
+}
+
+Tictactoe.prototype.checkWin = function () {
+  if (
+    this.rowWin(this.letters) ||
+    this.columnWin(this.letters) ||
+    this.diagonalWin(this.letters)
+  ) {
+    console.log("Winner!");
+    this.boxClicked.classList.add("container-no-click");
+    return;
+  } else if (
+    ((!this.rowWin(this.letters)) || !this.columnWin(this.letters) || !this.diagonalWin(this.letters)) &&
+    this.moves === 9
+  ) {
+    console.log("It is a tie!");
+    return;
   }
   return;
 }
